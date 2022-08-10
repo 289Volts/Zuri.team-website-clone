b2a3b8d32fd1104537b05cb363558f5e37932628
289Volts <289volts@gmail.com>
2022-08-10 16:36:03 +0100

    .

--- training.js
+++ training.js
@@ -1,68 +1,33 @@
-// the modal
-const modal = document.getElementById('modal-container');
-const modalB = document.getElementsByClassName('modal-container-Two');
-const modalC = document.getElementsByClassName('modal-container-Three');
-const ModalD = document.getElementsByClassName('modal-container-Four');
-
-// button to open the modal
-const openModal = document.getElementById('info1');
-const openModalTwo = document.getElementById('info2');
-const openModalThree = document.getElementById('info3');
-const openModalFour = document.getElementById('info4');
-
-// button to close the modal
-const closeModal = document.getElementById("close-btn");
-const closeModalTwo = document.getElementById("close-btn2");
-const closeModalThree = document.getElementById("close-btn3");
-const closeModalFour = document.getElementById("close-btn4");
-
-// When the user clicks on the button, open the modal
-openModal.onclick = function () {
-    modal.style.display = "block";
-}
-openModalTwo.onclick = function () {
-    modalB.style.display = "block";
-}
-openModalThree.onclick = function () {
-    modalC.style.display = "block";
-}
-openModalFour.onclick = function () {
-    ModalD.style.display = "block";
-}
+// const modal = document.getElementById("modal-container");
+// const modal2 = document.getElementsByClassName("modal-container-Two");
 
 
-// When the user clicks on X, close the modal
-closeModal.onclick = function () {
-    modal.style.display = "none";
-}
-closeModalTwo.onclick = function () {
-    modalB.style.display = "none";
-}
-closeModalThree.onclick = function () {
-    modalC.style.display = "none";
-}
-closeModalFour.onclick = function () {
-    ModalD.style.display = "none";
-}
+// function openModal() {
+//     modal.style.display = "block";
+//     console.log('work');
+// }
+// function openModal2() {
+//     modal2.style.display = "block";
+//     console.log('work1');
+// }[1]
+// function openModal3()  {
+//     console.log('work2');
+// }
+// function openModal4() {
+//     console.log('work3');
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-window.onclick = function (event) {
-    if (event.target == modalB) {
-        modalB.style.display = "none";
-    }
-}
-window.onclick = function (event) {
-    if (event.target == ModalC) {
-        modalC.style.display = "none";
-    }
-}
-window.onclick = function (event) {
-    if (event.target == ModalD) {
-        ModalD.style.display = "none";
-    }
+
+const triggers = document.getElementsByClassName("info-btn");
+const triggerArray = Array.from(triggers).entries();
+const modals = document.getElementsByClassName("modal");
+const closeButtons = document.getElementsByClassName("closebtn");
+
+for (let [index, trigger] of triggerArray) {
+	const toggleModal = () => {
+		// modals[index].classList.toggle("show-modal");
+		modals[index].style.display = "block";
+	};
+	// trigger.addEventListener("click", toggleModal);
+	// closeButtons[index].addEventListener("click", toggleModal);
 }
