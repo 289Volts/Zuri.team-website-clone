5048b780f1cf34f5965b9b86505ebe2837e88443
289Volts <289volts@gmail.com>
2022-08-14 07:54:54 +0100

    Job 99% done. MQ for all screens done. Only modal for Talent page uncompleted

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
-// const modalRed = document.getElementById("modal-container");
-const modalContentGray = document.querySelector('#modal-container2');
-// const buttonRed = document.getElementsByClassName("openModalhire");
-const buttonGray = document.getElementsByClassName("openmodalhiregray");
-// const close = document.querySelector('.hide-modal')
 
-// buttonRed.onclick = function () {
-//     modalRed.style.display = "block";
-// }
-
-buttonGray.onclick = function () {
-    modalContentGray.style.display = "block";
-}
-
-// window.onclick = function (event) {
-// 	if (event.target == modalRed) {
-// 		modal.style.display = "none";
-// 	}
-// };
